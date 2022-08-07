e68a2d42249b2fbe2b4926ec55b85b9e113792dc
Varris <varris@posteo.net>
2022-08-07 16:46:46 +0200

    test

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My personal dotfiles";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+
+      lib = nixpkgs.lib;
+
+    in
+    {
+      nixosConfigurations = {
+        terra = lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            ./system/configuration.nix
+          ];
+        };
+      };
+    };
+}
