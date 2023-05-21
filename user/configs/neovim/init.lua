b4e3cb1cdf998d09f72c93c039a4f76bcd38f42d
Varris <varris@posteo.net>
2023-05-21 23:52:56 +0200

    update

--- user/configs/neovim/init.lua
+++ user/configs/neovim/init.lua
@@ -105,7 +105,7 @@ require('nvim-surround').setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require("lspconfig")
 lspconfig.nil_ls.setup {
-  capabilities = capabilities
+  capabilities = capabilities,
 }
 lspconfig.lua_ls.setup {
   capabilities = capabilities,
